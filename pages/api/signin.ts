bc27047f43b226491d46af3bd1785f8675780ffe
AndriPurnomo <andri.adrp@gmail.com>
2023-01-14 21:55:03 +0700

    feat: signin API

---
+++ pages/api/signin.ts
@@ -0,0 +1,44 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import prisma from "../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user.id,
+        time: Date.now(),
+      },
+      "hello",
+      { expiresIn: "8h" },
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      }),
+    );
+    res.json(user);
+  } else {
+    res.json({ error: "Email or password wrong" });
+  }
+}
