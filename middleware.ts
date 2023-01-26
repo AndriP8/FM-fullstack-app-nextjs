b94f7059afcfa2953c31d22fde7002400556a168
AndriPurnomo <andri.adrp@gmail.com>
2023-01-26 21:25:05 +0700

    fix: get cookies middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req: any) {
   if (signedinPages.find((v) => v === req.nextUrl.pathname)) {
-    const token = req.cookies.get("TRAX_ACCESS_TOKEN").value;
+    const token = req.cookies.get("TRAX_ACCESS_TOKEN")?.value;
 
     if (!token) {
       return NextResponse.rewrite(new URL("/signin", req.url));
